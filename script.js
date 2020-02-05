2c62eeb7e3f3ab2640368a6ab8f9b6ef920a10ef
MD. Reyad Hossain <hassanhossain43@gmail.com>
2020-02-05 20:23:25 +0600

    ASDDSASD

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var origin = window.location.origin;
+var origin = window.location.hostname;
 var port =   window.location.port || 3000; // window.location.port ||
 console.log(origin);
-const socket = io(); //,{secure:true, reconnection: true,  rejectUnauthorized: false} 'http://'+ origin +':' + port +':' + port 'https://'+
+const socket = io(); //,{secure:true, reconnection: true,  rejectUnauthorized: false} 'http://'+ origin +':' + port 
 
 const msg_form = document.getElementById("msg_typesection");
 const msg_input = document.getElementById("msg_input");
